a8c2d347d2e2db4ee50b5a414649d25b81787064
ravenbbs <julioc3bgud@gmail.com>
2024-05-13 22:17:10 -0500

    add auth from authjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import node from "@astrojs/node";
+
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: node({
     mode: "standalone"
-  })
+  }),
+  integrations: [auth()]
 });
