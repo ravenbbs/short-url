0e03e915fd8cf2d3c0c3757550b1723c29be3c77
ravenbbs <julioc3bgud@gmail.com>
2024-05-18 09:14:41 -0500

    Add utils for get users - email

---
+++ src/pages/utils/db.ts
@@ -0,0 +1,28 @@
+import { db, eq, like, Link, User } from "astro:db";
+
+export const getUserByEmail = async (email:string) => {
+  try {
+    const res = await db.select().from(User).where(
+      like(User.email, email)
+    )
+  
+    if (res.length === 0) {
+      return {
+        success: true,
+        data: null
+      }
+    }
+
+    return {
+      success: true,
+      data: res[0]
+    }
+  } catch (e) {
+    const error = e as Error
+    return {
+      success: false,
+      error: error.message
+    }
+  }
+}
+
